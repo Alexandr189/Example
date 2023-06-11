2fb56cdd136fe72de6504c0f57912e0067ec0796
Alex <sanya.bula.89@gmail.com>
2023-06-11 11:22:26 +0700

    add Examples005_if_else

---
+++ Examples/Examples005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="санёк")
+{
+    System.Console.WriteLine("Снова ты!!");
+}
+else
+{
+    System.Console.WriteLine("Здорова, ");
+    System.Console.WriteLine(username); 
+}

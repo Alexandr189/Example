af53abe9f5edc4163127525eae6a644a8eae5dc6
Alex <sanya.bula.89@gmail.com>
2023-06-20 21:18:12 +0700

    Added Work005

---
+++ Examples/Examples009/Program.cs
@@ -0,0 +1,15 @@
+﻿// System.Console.Write("Введите искомое число : ");
+// int finde = Convert.ToInt32(Console.ReadLine());
+
+
+int [] array = {11,21,31,41,15,61,17,18,19};
+int number = array.Length;
+
+// index = 0;
+
+// if (array[index]=finde)
+// {
+//     System.Console.WriteLine();
+// }
+
+System.Console.WriteLine(number);

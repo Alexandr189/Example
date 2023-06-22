60ba6228b488edf9ab6e34b35d6471fc5edd4cd3
Alex <sanya.bula.89@gmail.com>
2023-06-22 21:50:06 +0700

    Added Work007

---
+++ Seminar/Work007/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {12,33,55,66};
+// Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+System.Console.WriteLine(string.Join(", ", array));
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " ");
+// }

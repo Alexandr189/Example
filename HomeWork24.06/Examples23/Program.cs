8214e237fe6c288dc4c1bd1656e53681b25a2730
Alex <sanya.bula.89@gmail.com>
2023-06-24 14:50:13 +0700

    Added folder Example23

--- HomeWork24.06/Examples23/Program.cs
+++ HomeWork24.06/Examples23/Program.cs
@@ -2,3 +2,16 @@
 // и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+
+System.Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+
+for(int i = 1; i<= number; i++)
+{
+    array[i-1]= (int) Math.Pow(i,3);
+}
+
+System.Console.WriteLine("[" + string.Join(", ", array)+ "]");

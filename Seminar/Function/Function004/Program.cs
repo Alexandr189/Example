27e4d4b69d62253fa4654a550e2d451ac8554fa7
Alex <sanya.bula.89@gmail.com>
2023-06-27 15:25:37 +0700

    Added Function004

--- Seminar/Function/Function004/Program.cs
+++ Seminar/Function/Function004/Program.cs
@@ -8,28 +8,30 @@ void FilArray(int[] arr)
 {
     Random rand = new Random();
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-       arr[i] = rand.Next(0, 2);
-    }   
+        arr[i] = rand.Next(0, 2);
+    }
 
 
 }
 
 void PrintArray(int[] arr)
 {
-    System.Console.WriteLine(string.Join(" ", array));
+    System.Console.WriteLine(string.Join(" ", arr));
+}
+
+// Функция ввода данных
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
 
-
-
-
-
-
 //------------------------------------------------
-
-int[] array = new int[8];
+int SizeArray = ReadInt("Введите размер массива: ");
+int[] array = new int[SizeArray];
 FilArray(array);
 PrintArray(array);
 

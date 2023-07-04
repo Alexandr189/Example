0669b2a67077313613088e51dfdf306032ea297a
Alex <sanya.bula.89@gmail.com>
2023-07-04 21:07:06 +0700

    Added Method ReversArray

---
+++ Methods/ReversArray/Program.cs
@@ -0,0 +1,12 @@
+﻿void ReversArray(int[] array)
+{
+    int temp = 0;
+    for(int i = 0; i<array.Length/2; i++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length-1-i];
+        array[array.Length-1-i] = temp;
+    }
+}
+
+//Array.Reverse(array); Вариант разворота при помощи встроенной функции

a0c071cd56154e2667c8bfa737e2ff71a7950b79
Alex <sanya.bula.89@gmail.com>
2023-07-06 23:30:51 +0700

    Added HomeWork08.07 number 50

--- HomeWork/HomeWork08.07/Task50/Program.cs
+++ HomeWork/HomeWork08.07/Task50/Program.cs
@@ -6,3 +6,61 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PrintNumberMatrix(int [,] matrix , int rows , int cols)
+{
+    if(rows>matrix.GetLength(0) || cols > matrix.GetLength(1))
+    {
+        System.Console.WriteLine("Такого числа нет");
+    }
+    else System.Console.WriteLine(matrix[rows-1,cols-1]);
+        
+    
+
+}
+//------------------------------------------------------
+
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+int[,] matrix = FillMatrix(rows, cols, 0, 100);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+int numberRows = ReadInt("Введите номер строки: ");
+int numberCols = ReadInt("Введите номер столбца: ");
+PrintNumberMatrix(matrix,numberRows,numberCols);

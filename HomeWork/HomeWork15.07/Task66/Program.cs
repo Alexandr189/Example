23c97d961ce3f3a140a24b418753f0fcf8533263
Alex <sanya.bula.89@gmail.com>
2023-07-14 10:49:03 +0700

    Added HomeWork15.07 : Task66, Task68

---
+++ HomeWork/HomeWork15.07/Task66/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void Main()
+{
+int M = ReadInt("Введите начальное число: ");
+int N = ReadInt("Введите конечное число: ");
+int sum = Summ(M,N);
+System.Console.WriteLine($"Сумма натуральных чисел от числа {M} до {N} = {sum + M}");
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// вывод натуральных чисел в промежутке от M до N
+void Numbers(int M, int N)
+{
+    if (N < M) return;
+    System.Console.Write(M + " ");
+    Numbers(M + 1, N);
+
+}
+int Summ(int M, int N)
+{
+    int sum = M;
+        if (M == N) 
+        return 0;
+        else
+        {
+            M++;
+            sum = M + Summ(M , N);
+            return sum;
+        }
+
+}
+//----------------------------------------------------------------
+
+Main();
---
+++ HomeWork/HomeWork15.07/Task68/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+void Main()
+{
+int M = ReadInt("Введите  число M: ");
+int N = ReadInt("Введите число N: ");
+int akk = AkkerMan(M,N);
+System.Console.WriteLine($"Функция Аккермана от чисел {M} и {N} равна {akk}");
+}
+
+int AkkerMan(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return AkkerMan(m - 1, 1);
+    }
+    else
+    {
+        return (AkkerMan(m - 1, AkkerMan(m, n - 1)));
+    }
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs (Convert.ToInt32(Console.ReadLine()));
+}
+//-----------------------------------------------
+Main();

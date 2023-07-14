77faf7d4fc97137afa11a30f6c260295cb384ffd
Alex <sanya.bula.89@gmail.com>
2023-07-14 13:59:08 +0700

    Added HomeWork16.07 , Task54 , Task62

---
+++ HomeWork/HomeWork16.07/Task62/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+
+void Main()
+{
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введитеколичество столбцов: ");
+if(rows!= cols)
+{
+    System.Console.WriteLine("Количество строк не равно количеству столбцов");
+}
+else
+PrintMatrix ( FillMatrix(rows,cols));
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] FillMatrix(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return matrix;
+
+}
+//-----------------------------------------------------------------------------
+
+Main();
